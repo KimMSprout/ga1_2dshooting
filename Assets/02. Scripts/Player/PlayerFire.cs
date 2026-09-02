17337e64141237d94f7841b2947dcf915641dcb1
Kim Min Seok <100112596+KimMSprout@users.noreply.github.com>
2026-09-02 14:29:35 +0900

    feat: 총알 발사 기능 추가

---
+++ Assets/02. Scripts/Player/PlayerFire.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlayerFire : MonoBehaviour
+{
+    // 목표: 스페이스바를 누를 때마다 총알을 생성해서 발사하고 싶다.
+    // 필요 속성
+    // - 총알 프리팹
+    public GameObject BulletPrefab;
+    public Transform FirePointLeft;
+    public Transform FirePointRight;
+    // - 생성 위치(총구)
+    
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        // 1. 스페이스바를 누르면
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // 2. 총알 프리팹을 생성한다.
+            // Instantiate는 프리팹을 복사해서 (MonoBehavior를 상속받는) 게임 오브젝트를 생성하고 씬에 넣어주는 기능
+            // 클래스로부터 객체를 만드는 과정을 인스턴트화(Instantiate)
+            GameObject bulletLeft = Instantiate(BulletPrefab);
+            bulletLeft.transform.position = FirePointLeft.position;
+            
+            GameObject bulletRight = Instantiate(BulletPrefab);
+            bulletRight.transform.position = FirePointRight.position;
+        }
+    }
+}
