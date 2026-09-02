d8167503a5c83894fb9d88d81b40460b588b36de
Kim Min Seok <100112596+KimMSprout@users.noreply.github.com>
2026-09-02 14:29:17 +0900

    Refactor: 플레이어 이동, 키보드 입력 함수 리팩토링

--- Assets/02. Scripts/Player/PlayerMove.cs
+++ Assets/02. Scripts/Player/PlayerMove.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Diagnostics;
+using Debug = UnityEngine.Debug;
 
 public class PlayerMove : MonoBehaviour
 {
@@ -12,11 +14,27 @@ public class PlayerMove : MonoBehaviour
     float borderRight = 2.3f;
     float borderUp = 0f;
     float borderUnder = -5f;
-    
+    private Command _ICommand;
+
+    public void Start()
+    {
+        Command command = new Command();
+    }
+
+
     // 매 프레임마다 실행된다.
     // 초당 프레임 실행 횟수는 : 별다른 설정이 없을 경우 가능한 많이 (컴퓨터 성능에 따라, 환경에 따라 다름)
-    
+
     public void Update()
+    {
+        Move();
+        
+        SpeedChange();
+        
+
+    }
+
+    public void Move()
     {
         // 1. 키보드 입력을 받는다.
         // 1.1 
@@ -24,15 +42,15 @@ public class PlayerMove : MonoBehaviour
         // {
         //     
         // }
-        
+
         // Debug.Log("왼쪽 방향키를 누르는 중");
-        
+
         // 1.2 
         float h = Input.GetAxisRaw("Horizontal"); // 키보드 좌/우 입력 상태에 따라 -1f ~ 1f
         float v = Input.GetAxisRaw("Vertical"); // 키보드 위/아래 입력 상태에 따라 -1f ~ 1f
-        
+
         Debug.Log($"h:{h}, v:{v}");
-        
+
         // 실습 과제 1
         // if ((h != 0) && (transform.position.x < borderLeft || transform.position.x > borderRight))
         // {
@@ -43,14 +61,14 @@ public class PlayerMove : MonoBehaviour
         // {
         //     v = v * -1;
         // }
-        
+
         // 실습 과제 2
         if ((h != 0) && (transform.position.x < borderLeft || transform.position.x > borderRight))
         {
             transform.position = new Vector2(transform.position.x * -1, transform.position.y);
         }
-        
-        if((v != 0) && (transform.position.y > borderUp || transform.position.y < borderUnder))
+
+        if ((v != 0) && (transform.position.y > borderUp || transform.position.y < borderUnder))
         {
             if (transform.position.y > borderUp)
             {
@@ -61,37 +79,42 @@ public class PlayerMove : MonoBehaviour
                 transform.position = new Vector2(transform.position.x, borderUp);
             }
         }
-        
+
         Vector2 direction = new Vector2(h, v);
-        
-        // 실습 과제 3
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            SpeedUpDown(KeyCode.Q);
-        }
-        else if (Input.GetKeyDown(KeyCode.E))
-        {
-            SpeedUpDown(KeyCode.E);
-        } 
+
         // 2. 키보드 입력에 따라 방향을 구한다.
         // 게임에는 벡터라는 타입이 있다. 벡터는 크기와 방향을 의미한다.
         // Vector2 direction = new Vector2(h, v); // 왼쪽 방향
         // Vector2 dircetion = Vector2.left; 내부적으로는 위와 같은 값
         // GameObject의 위치를 바꾸려면 Transform의 Position을 바꾸어 주면 된다.
-            
-            
+
+
         // 3. 방향과 속도에 따라 이동한다.
         // 속도 = 방향 * 속력                      // 매직 넘버 : 보는 사람에 따라 의미가 달라질 수 있는 헷갈리는 숫자
 
         Vector2 normalizedSpeed = direction.normalized * Speed;
-        transform.Translate( normalizedSpeed * Time.deltaTime);
-        // deltaTime : 이전 프레임으로부터 지금 프레임까지 시간이 얼마나 지났는지 MS(밀리세컨즈)로 반환
-        
-        // 새로운 위치 = 현재 위치 + (방향 * 속력 * 시간)
-        // transform.position += new Vector2(transform.position + (Vector3)direction * Speed * Time.deltaTime);
+        transform.Translate(normalizedSpeed * Time.deltaTime);
     }
 
-    public void SpeedUpDown(KeyCode key)
+    public void BulletFire()
+    {
+        
+    }
+
+    public void SpeedChange()
+    {
+        // 실습 과제 3
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            KeyboardCommand(KeyCode.Q);
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            KeyboardCommand(KeyCode.E);
+        } 
+    }
+
+    public void KeyboardCommand(KeyCode key)
     {
         if (key == KeyCode.E)
         {
