4b5bf4815b84c4927a6cf8474ed9a4c38053f3ec
Kim Min Seok <100112596+KimMSprout@users.noreply.github.com>
2026-09-02 14:57:14 +0900

    feat: 보조 총알(쿨타임, 속도, 모양이 다름) 발사 기능 추가

---
+++ Assets/02. Scripts/Bullet/SubBulletMove.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class SubBulletMove : MonoBehaviour
+{
+    public float Speed;
+    
+    private void Update()
+    {
+        Move();       
+    }
+
+    public void Move()
+    {
+        Vector2 direction = Vector2.up;
+        transform.Translate(direction * Speed * Time.deltaTime);
+    }
+}
--- Assets/02. Scripts/Player/PlayerFire.cs
+++ Assets/02. Scripts/Player/PlayerFire.cs
@@ -6,13 +6,20 @@ public class PlayerFire : MonoBehaviour
     // 필요 속성
     // - 총알 프리팹
     public GameObject BulletPrefab;
+    public GameObject SubBulletPrefab;
 
     // - 생성 위치(총구)
     public Transform FirePointLeft;
-    public Transform FirePointRight;
+    public Transform FirePointRight;    
+    
+    public Transform SubFirePointLeft;
+    public Transform SubFirePointRight;
 
     public float FireRate;
     public float FireCoolTime = 0;
+    
+    public float SubFireRate;
+    public float SubFireCoolTime = 0;
 
     public  bool isAuto = false;
         
@@ -25,20 +32,36 @@ public class PlayerFire : MonoBehaviour
     private void Fire()
     {
         FireCoolTime += Time.deltaTime;
+        SubFireCoolTime += Time.deltaTime;
 
         // 1. 스페이스바를 누르면
-        if ((Input.GetKeyDown(KeyCode.Space) || isAuto) && FireCoolTime >= FireRate)
+        if ((Input.GetKeyDown(KeyCode.Space) || isAuto))
         {
-            // 2. 총알 프리팹을 생성한다.
-            // Instantiate는 프리팹을 복사해서 (MonoBehavior를 상속받는) 게임 오브젝트를 생성하고 씬에 넣어주는 기능
-            // 클래스로부터 객체를 만드는 과정을 인스턴트화(Instantiate)
-            GameObject bulletLeft = Instantiate(BulletPrefab);
-            bulletLeft.transform.position = FirePointLeft.position;
+            if(FireCoolTime >= FireRate)
+            {
+                // 2. 총알 프리팹을 생성한다.
+                // Instantiate는 프리팹을 복사해서 (MonoBehavior를 상속받는) 게임 오브젝트를 생성하고 씬에 넣어주는 기능
+                // 클래스로부터 객체를 만드는 과정을 인스턴트화(Instantiate)
+                GameObject bulletLeft = Instantiate(BulletPrefab);
+                bulletLeft.transform.position = FirePointLeft.position;
 
-            GameObject bulletRight = Instantiate(BulletPrefab);
-            bulletRight.transform.position = FirePointRight.position;
+                GameObject bulletRight = Instantiate(BulletPrefab);
+                bulletRight.transform.position = FirePointRight.position;            
 
-            FireCoolTime = 0;
+                FireCoolTime = 0;
+            }
+
+            if (SubFireCoolTime >= SubFireRate)
+            {
+                GameObject subBulletLeft = Instantiate(SubBulletPrefab);
+                subBulletLeft.transform.position = SubFirePointLeft.position;
+
+                GameObject subBulletRight = Instantiate(SubBulletPrefab);
+                subBulletRight.transform.position = SubFirePointRight.position;
+
+                SubFireCoolTime = 0;
+            }
+
         }
     }
 
