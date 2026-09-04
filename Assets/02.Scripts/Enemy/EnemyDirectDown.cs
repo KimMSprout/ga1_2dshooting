6f814ee1b89bd0e51e27f576fa259072ad5a38a8
Kim Min Seok <100112596+KimMSprout@users.noreply.github.com>
2026-09-04 11:17:45 +0900

    test: Test를 위한 Find 함수 추가

--- Assets/02.Scripts/Enemy/EnemyDirectDown.cs
+++ Assets/02.Scripts/Enemy/EnemyDirectDown.cs
@@ -14,6 +14,7 @@ public class EnemyDirectDown : Enemy
     public void Update()
     {
         Move();
+        _player = GameObject.FindGameObjectWithTag("Player");
     }
 
     protected override void Move()
