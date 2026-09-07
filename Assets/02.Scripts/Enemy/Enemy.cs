baf64d82d2d71f1773b91804ff2b0c7956087eac
Kim Min Seok <100112596+KimMSprout@users.noreply.github.com>
2026-09-07 14:12:35 +0900

    asset: Shooting Pack Asset Import

--- Assets/02.Scripts/Enemy/Enemy.cs
+++ Assets/02.Scripts/Enemy/Enemy.cs
@@ -23,6 +23,11 @@ public abstract class Enemy : MonoBehaviour
         }
     }
 
+
+    // Todo : Scriptable Object를 사용해서 리팩토링
+    // 이유 1: 배열을 사용했지만 각 아이템이 어떤 프리팹인지 알 수가 없음
+    // 이유 2: 각 에너미 스폰 확률을 매직 넘버로 하드코딩해서 유지보수가 어렵다.
+
     private void SpawnItem()
     {
         int random = UnityEngine.Random.Range(1, 100 + 1);
