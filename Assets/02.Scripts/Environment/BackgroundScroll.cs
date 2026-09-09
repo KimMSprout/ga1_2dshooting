b3b8c57f3f01f4b0ee5bc19c0934f8769aa34248
Kim Min Seok <100112596+KimMSprout@users.noreply.github.com>
2026-09-09 14:11:28 +0900

    chore: 배경 이미지 변경

--- Assets/02.Scripts/Environment/BackgroundScroll.cs
+++ Assets/02.Scripts/Environment/BackgroundScroll.cs
@@ -15,6 +15,8 @@ public class BackgroundScroll : MonoBehaviour
     private void Update()
     {
         _offsetY += _scrollSpeed * Time.deltaTime;
+
+        // TODO: 머터리얼 프로퍼티 블록을 통한 최적화
         _material.mainTextureOffset = new Vector2(0, _offsetY);
     }
 }
